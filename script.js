f9d4b1560fcb853b778da6590c47ef8f59f7632c
Ty Hunt <jtmhunt22@gmail.com>
2020-07-08 21:24:08 -0600

    Changed up some of the javascript still need to figure out local storage and the icons for each day.

--- script.js
+++ script.js
@@ -1,61 +1,60 @@
-var citiesSearched = JSON.parse(localStorage.getItem("searchHistory"));
-
-
-function displayWeather(event){
-     event.preventDefault();
-     var userLocation = $("#searchInput").val()
-     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=cf9b5b3a1e8e73235be6a1620503c21f&units=imperial` ;
+function displayWeather(event) {
+    event.preventDefault();
+    var userLocation = $("#searchInput").val()
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=cf9b5b3a1e8e73235be6a1620503c21f&units=imperial`;
     $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-     var date = moment().format("dddd, MMMM Do")
-      $("#cityName").text(response.name)
-      var iconCode = response.weather[0].icon;
-      var iconURL  = "http://openweathermap.org/img/w/" + iconCode + ".png";
-     $("#currentTime").text(date)
-      $("#currentIcon").attr('src', iconURL)
-     $("#currentTemp").text("Temperature: " + response.main.temp + "F")
-      $("#currentHumidity").text("Humidity: " + response.main.humidity + "%")
-      $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed + " MPH")
-      displayUVIndex(response.coord.lat, response.coord.lon)
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var date = moment().format("dddd, MMMM Do")
+        $("#cityName").text(response.name)
+        var iconCode = response.weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png";
+        $("#currentTime").text(date)
+        $("#currentIcon").attr('src', iconURL)
+        $("#currentTemp").text("Temperature: " + response.main.temp + "F")
+        $("#currentHumidity").text("Humidity: " + response.main.humidity + "%")
+        $("#currentWindSpeed").text("Wind Speed: " + response.wind.speed + " MPH")
+        displayUVIndex(response.coord.lat, response.coord.lon)
 
-      var cities =  $("#searchInput").val()
-      citiesSearched.push(cities)
-      localStorage.setItem("searchHistory", JSON.stringify(citiesSearched))
-     })
- }
+        // ! need to set the local storage for each item
+        // var cities = $("#searchInput").val()
+        // citiesSearched.push(cities)
+        // localStorage.setItem("searchHistory", JSON.stringify(citiesSearched))
+        // var citiesSearched = localStorage.getItem('searchHistory') ? JSON.parse(localStorage.getItem("searchHistory")) : [];
 
- function displayUVIndex(lat, lon){
-    var queryURL = `https://api.openweathermap.org/data/2.5/uvi?&appid=cf9b5b3a1e8e73235be6a1620503c21f&units=imperial&lat=${lat}&lon=${lon}` ;
-   $.ajax({
-     url: queryURL,
-     method: "GET"
-   }).then(function(response) {
+        $("#firstDisplay").addClass("hide")
+    })
+}
+
+function displayUVIndex(lat, lon) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/uvi?&appid=cf9b5b3a1e8e73235be6a1620503c21f&units=imperial&lat=${lat}&lon=${lon}`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
         $("#currentUVIndex").text("UV Index: " + response.value)
-   })
- }
+    })
+}
 
- function displayFiveDayWeather(event){
+function displayFiveDayWeather(event) {
     event.preventDefault();
     var userLocation = $("#searchInput").val()
     var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${userLocation}&appid=cf9b5b3a1e8e73235be6a1620503c21f&units=imperial`;
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
+    }).then(function (response) {
         // console.log(response)
-        // ! how do i get each day to be on the screen
         var results = response.list
-        console.log(results);
-        
+        // console.log(results);
+        //this gets the weather for the next 5 days
         $(".row").empty()
-        for(var i = 0; i < results.length; i += 8){
-          var dates =  results[i].dt_txt.split(" ")[0]
-            var col = $("<div class = 'col-lg-3'>") 
+        for (var i = 0; i < results.length; i += 8) {
+            var dates = results[i].dt_txt.split(" ")[0]
+            var col = $("<div class = 'col-lg-3'>")
             var card = $("<div class= 'card'>")
             var cardBody = $("<div class= 'card-body'>")
-            var date = $("<h3>")
             // var futureImg = results.weather.icon;
             // console.log(futureImg)
             // var futureIconUrl = "http://openweathermap.org/img/w/" + futureImg + ".png";
@@ -74,15 +73,13 @@ function displayWeather(event){
             //push everything to the screen
             col.append(card)
             card.append(cardBody)
-            cardBody.append(nextDate, dailyTemp, dailyHumidity, dailyWind,)
+            cardBody.append(nextDate, dailyTemp, dailyHumidity, dailyWind, )
             $(".row").append(col)
 
         }
-     
+
     })
-    
+
 }
 $("#searchBtn").on("click", displayWeather)
 $("#searchBtn").on("click", displayFiveDayWeather)
-
-
